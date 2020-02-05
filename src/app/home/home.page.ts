4a06979e857c88aa11f2cd0cb86a026825923333
drpratik <pratikdhore7@gmail.com>
2020-02-05 13:26:48 +0530

    confirm box added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,45 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public alertController: AlertController) {}
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Confirm',
+      message: 'Are you sure to cancel appointment?',
+      buttons: ['NO','YES']
+    });
 
+    await alert.present();
+  }
+
+  async presentAlertConfirm() {
+    const alert = await this.alertController.create({
+      header: 'Confirm!',
+      message: 'Are you sure to cancel <strong>appointment?</strong>',
+      buttons: [
+        {
+          text: 'No',
+          role: 'no',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Yes',
+          handler: () => {
+            console.log('Confirm Okay');
+           
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+  data={
+    list:[
+      {booked:1,date:'26-2-2019',day:'Wednesday',slot:'12:00 PM',bname:'Cancel appointment'}
+    ],
+  };  
+  
 }
